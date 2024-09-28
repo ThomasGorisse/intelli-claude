e45cf224f335a3b2cf3bd6f585725f3e86966462
GitHub Action <action@github.com>
2024-09-28 14:42:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/thomasgorisse/intelliclaude/MyBundle.kt
+++ src/main/kotlin/com/github/thomasgorisse/intelliclaude/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.thomasgorisse.intelliclaude
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/thomasgorisse/intelliclaude/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/thomasgorisse/intelliclaude/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.thomasgorisse.intelliclaude.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
